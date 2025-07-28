bff6e0f31eabea08b1fc8236573ef61f5e6473e4
Caleb Kimmel <63613771+caleb-kimmel@users.noreply.github.com>
2025-07-28 12:36:09 -0500

    Update grabbing_seqs.sh

--- grabbing_seqs.sh
+++ grabbing_seqs.sh
@@ -30,10 +30,27 @@ wget "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=JA
 
 wget "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=JARFTL010000010.1&rettype=fasta&retmode=text" -O genome/scaf10.fasta
 
+# getting the genome files in .gbk format so that antismash will run
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/014/930/995/GCF_014930995.1_ASM1493099v1/GCF_014930995.1_ASM1493099v1_genomic.gbff.gz && \
+gzip -d GCF_014930995.1_ASM1493099v1_genomic.gbff.gz && \
+mv GCF_014930995.1_ASM1493099v1_genomic.gbff genome/PMA22_genome.gbk
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/038/447/645/GCF_038447645.1_ASM3844764v1/GCF_038447645.1_ASM3844764v1_genomic.gbff.gz && \
+gzip -d GCF_038447645.1_ASM3844764v1_genomic.gbff.gz && \
+mv GCF_038447645.1_ASM3844764v1_genomic.gbff genome/IMGN2_genome.gbk
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/009/225/GCF_000009225.2_ASM922v1/GCF_000009225.2_ASM922v1_genomic.gbff.gz && \
+gzip -d GCF_000009225.2_ASM922v1_genomic.gbff.gz && \
+mv GCF_000009225.2_ASM922v1_genomic.gbff genome/SBW25_genome.gbk
+
+efetch -db nucleotide -format gbwithparts -id AY061859.1 > genome/safBGC.gbk
+
 # changing the header of these fasta files so my code works in the future
 sed -i 's/^>.*/>safBGC/' genome/safBGC.fasta
 sed -i 's/^>.*/>scaf04/' genome/scaf04.fasta
 sed -i 's/^>.*/>scaf10/' genome/scaf10.fasta
+sed -i 's/^>.*/>IMGN2_genome/' genome/IMGN2_genome.fasta
+sed -i 's/^>.*/>SBW25_genome/' genome/SBW25_genome.fasta
 
 # creating the individual safracin gene files by using the coords listed in its genbank page and just snipping it out.
 samtools faidx genome/safBGC.fasta
@@ -47,3 +64,12 @@ samtools faidx genome/safBGC.fasta safBGC:15933-16976 > genes/sfcG.fasta
 samtools faidx genome/safBGC.fasta safBGC:17062-17604 > genes/sfcH.fasta
 samtools faidx genome/safBGC.fasta safBGC:1669-2331 > genes/sfcI.fasta
 samtools faidx genome/safBGC.fasta safBGC:150-1679 > genes/sfcJ.fasta
+
+# creating the files for the SBW25 and IMGN2 regions of interest the same way.
+samtools faidx genome/IGMN2_genome.fasta
+samtools faidx genome/SBW25_genome.fasta
+samtools faidx genome/IGMN2_genome.fasta IMGN2_genome:4011063-4094138 > genome/IMGN2_reg08.fasta
+samtools faidx genome/IGMN2_genome.fasta IMGN2_genome:4834352-4887236 > genome/IMGN2_reg11.fasta
+samtools faidx genome/IGMN2_genome.fasta SBW25_genome:2732627-2838158 > genome/SBW25_reg04.fasta
+samtools faidx genome/IGMN2_genome.fasta SBW25_genome:3508015-3584310 > genome/SBW25_reg05.fasta
+samtools faidx genome/IGMN2_genome.fasta SBW25_genome:4820788-4873678 > genome/SBW25_reg11.fasta
